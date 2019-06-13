798e9da374f9f945be1e82e5d34f022810a902c8
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-13 17:00:01 -0600

    adding animals

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Button } from 'semantic-ui-react'
 import {HobbiesContext, }from '../providers/UserHobbies'
 import {UserContext} from '../providers/UserProvider'
 
@@ -7,19 +7,28 @@ const Hobbies = () =>{
   const {hobbies} = useContext(HobbiesContext)
   const {firstName, lastName} =useContext(UserContext)
 
-return(
 
-  <Card >
-      <Card.Content>
-        <Card.Header>{hobbies}</Card.Header>
+
+  return (
+    <Card>
+      {hobbies.map(hobby =>(
+        <Card.Content>
+        <Card.Header>{hobby.name}</Card.Header>
       </Card.Content>
+     
+      ))}
       <Card.Content extra>
-       
-       {firstName} {lastName}
+        <Button>Add Hobbie</Button>
       </Card.Content>
     </Card>
+  )
+      }
+     
+     
 
-)
-}
+
+
+
+
 
 export default Hobbies
--- src/providers/UserHobbies.js
+++ src/providers/UserHobbies.js
@@ -6,7 +6,9 @@ export const HobbiesContext = React.createContext();
 export const HobbiesConsumer = HobbiesContext.Consumer
 
 class UserHobbies extends React.Component{
-  state = {hobbies:"Eating"}
+  state = {hobbies:[{id:1, name:"eating"},
+  {id:2, name: "sleeping"}]
+}
   render(){
     return(
       <HobbiesContext.Provider value={this.state}>
